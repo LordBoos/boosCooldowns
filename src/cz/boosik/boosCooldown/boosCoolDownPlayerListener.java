576d9d67b0f1e662376cf32b7518db835c40534a
Zak Ford <zak.j.ford@gmail.com>
2012-02-05 18:25:19 -0500

    Schedule warmup task in main thread instead of separate thread

--- src/cz/boosik/boosCooldown/boosCoolDownPlayerListener.java
+++ src/cz/boosik/boosCooldown/boosCoolDownPlayerListener.java
@@ -1,6 +1,6 @@
 package cz.boosik.boosCooldown;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashMap;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -17,8 +17,8 @@ import util.boosChat;
 @SuppressWarnings("deprecation")
 public class boosCoolDownPlayerListener extends PlayerListener {
 	private final boosCoolDown plugin;
-	private static ConcurrentHashMap<String, Location> playerloc = new ConcurrentHashMap<String, Location>();
-	private static ConcurrentHashMap<String, String> playerworld = new ConcurrentHashMap<String, String>();
+	private static HashMap<String, Location> playerloc = new HashMap<String, Location>();
+	private static HashMap<String, String> playerworld = new HashMap<String, String>();
 
 	public boosCoolDownPlayerListener(boosCoolDown instance) {
 		plugin = instance;
